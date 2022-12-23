fcb0bea9d7461b790c4734c574fdd7a2a669eeba
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-12-23 18:47:04 +0900

    router 생성

--- src/server.js
+++ src/server.js
@@ -1,14 +1,35 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const handleHome = (req, res) => res.send("hello");
-const handleLogin = (req, res) => res.send("Login");
+const globalRouter = express.Router();
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => {
+  return res.send("Edit User");
+};
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => {
+  return res.send("Watch Videos");
+};
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🎅`);
