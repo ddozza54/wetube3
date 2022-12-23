be8fcefe3f997daa684d9289117087a3d45994ec
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-12-23 19:30:50 +0900

    controller 분리

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,9 @@
 import express from "express";
+import { edit, remove } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-const handleEditUser = (req, res) => {
-  return res.send("Edit User");
-};
-
-userRouter.get("/edit", handleEditUser);
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
+import { watch, edit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-const handleWatchVideo = (req, res) => {
-  return res.send("Watch Videos");
-};
-videoRouter.get("/watch", handleWatchVideo);
+videoRouter.get("/watch", watch);
+videoRouter.get("/edit", edit);
 
 export default videoRouter;
