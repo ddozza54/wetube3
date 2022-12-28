5a9d57c1e74f4300623cb40d6cada4e95a675c0e
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-12-28 20:34:44 +0900

    pug로 변수 보내기, block 사용하기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
 export const trending = (req, res) => res.render("home");
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.send("Edit Videos");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload video");
 export const deleteVideo = (req, res) => res.send("delete video");
