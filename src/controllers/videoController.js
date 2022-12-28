09db432a42e2534d240639fe5875ab0fdc6fbe88
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-12-28 19:30:27 +0900

    pug의 include를 이용해 footer 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,5 @@
-export const trending = (req, res) => {
-    return res.render("home")};
-export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);};
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("Edit Videos");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload video");
