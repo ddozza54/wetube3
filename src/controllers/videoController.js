e222038be8c7d528aef6118d462ac0aa72b7373a
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-01-02 21:02:31 +0900

    ilteration, mixin, req.params, req.boby

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,40 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+const videos = [
+  {
+    title: "good Movie",
+    rate: 3,
+    views: 1,
+    owner: "happy man",
+    id: 1,
+  },
+  {
+    title: "Sad Movie",
+    rate: 80,
+    views: 13213,
+    owner: "Sad man",
+    id: 2,
+  },
+];
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
+
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  console.log(req.body);
+  return res.redirect(`/videos/${id}`);
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload video");
 export const deleteVideo = (req, res) => res.send("delete video");
--- src/server.js
+++ src/server.js
@@ -11,7 +11,8 @@ const logger = morgan("dev");
 app.use(logger);
 
 app.set("view engine", "pug");
-app.set("views", process.cwd()+"/src/views");
+app.set("views", process.cwd() + "/src/views");
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
