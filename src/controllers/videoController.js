ec730e807cd760cb438538ad29e499495a41b118
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-01-05 16:42:38 +0900

    watching page  - title, form 수정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,7 +8,7 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  return res.render("watch", { pageTitle: "watch", video });
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
 export const getEdit = (req, res) => {
