44ecea36886a6756397cba83b576f8aa39f07d63
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-01-10 02:20:52 +0900

    bcrypt - hash 와 pre('save'~) 로 암호화된 password 만들기

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,11 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = (req, res) => res.render();
+export const postJoin = async (req, res) => {
+  const { username, email, password, name, loacation } = req.body;
+  await User.create({ username, email, password, name, loacation });
+  return res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
