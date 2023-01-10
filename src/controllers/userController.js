6ae9dac5244317d7bf9b344485f5cd984085a549
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-01-10 14:52:22 +0900

    or 연산자를 이용해 중복되는 username, email 거르기

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,14 @@ import User from "../models/User";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   const { username, email, password, name, loacation } = req.body;
+  const pageTitle = "Join";
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
   await User.create({ username, email, password, name, loacation });
   return res.redirect("/login");
 };
