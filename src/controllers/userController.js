6cdd74b297b2fa74a5d9141a3e9739c37b5b05bf
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-02-19 21:06:22 +0900

    로그인 에러 수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -42,7 +42,7 @@ export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
 
 export const postLogin = async (req, res) => {
-  const { username, password } = res.body;
+  const { username, password } = req.body;
   const pageTitle = "Login";
   const user = await User.findOne({ username });
   if (!user) {
@@ -52,7 +52,7 @@ export const postLogin = async (req, res) => {
     });
   }
   const ok = await bcrypt.compare(password, user.password);
-  if (!match) {
+  if (!ok) {
     return res.status(400).render("login", {
       pageTitle,
       errorMessage: "Wrong Password",
