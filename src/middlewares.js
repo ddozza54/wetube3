cbc1b3b176337d939d98502fc01a34e7eab89d43
UHquokka <bananamayong@gmail.com>
2023-03-13 19:24:34 +0900

    middleware를 만들어서 loggedIn에 따른 보호

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,3 +1,5 @@
+import session from "express-session";
+
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   // req.session.loggedIn 얘는 postLoggedIn 에서 로그인 통과하면 보내줌.
@@ -5,4 +7,20 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
   next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
