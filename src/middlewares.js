e390d9c996049117f41d00bf73f29da967f33eb4
UHquokka <bananamayong@gmail.com>
2023-03-13 19:00:54 +0900

    edit user Form 만들기

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,6 +3,6 @@ export const localsMiddleware = (req, res, next) => {
   // req.session.loggedIn 얘는 postLoggedIn 에서 로그인 통과하면 보내줌.
   // 이제 전체 페이지에서 로그인 했는지 여부를 알 수 있음.
   res.locals.siteName = "Wetube";
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
   next();
 };
